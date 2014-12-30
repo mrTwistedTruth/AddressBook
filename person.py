d9b76be0ae9268b993d2f9b0458e8bcf88a5f742
mrTwistedTruth <mrtwistedtruth@gmail.com>
2014-12-30 18:02:02 +0000

    Commit Version 1.0

--- person.py
+++ person.py
@@ -7,6 +7,8 @@ class Person():
     def __init__(self, name):
         self.name = name
         self.phone = None
+        self.email = None
+        self.image = None
         print('Created contact {0}'.format(self.name))
 
     def __del__(self):
@@ -20,9 +22,17 @@ class Person():
     def getNumber(self):
         return self.phone
 
-    def createNewPerson(self, n, ph):
-       self.name = n
-       self.phone = ph
+    def getEmail(self):
+        return self.email
 
-    def updatePerson(self, ph):
-        self.phone = ph
+    def createNewPerson(self, name, phone, email, image):
+       self.name = name
+       self.phone = phone
+       self.email = email
+       self.image = image
+
+    def updatePerson(self, phone, email, image):
+        # Check if args equal to None and if so, leave them as they were.
+        self.phone = phone
+        self.email = email
+        self.image = image
